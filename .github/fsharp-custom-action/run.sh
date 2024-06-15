df32f1747edc9dc2b8063113c874c5cf7b265b94
64J0 <vinigaio97@gmail.com>
2024-06-15 15:54:49 -0300

    chore: revert the change on git config

--- .github/fsharp-custom-action/run.sh
+++ .github/fsharp-custom-action/run.sh
@@ -16,6 +16,8 @@ cd "${REPO_PATH}"
 
 ls -la
 
+# fatal: detected dubious ownership in repository at '/github/workspace'
+git config --global --add safe.directory /github/workspace
 git tag --sort -creatordate
 GIT_TAGS=$(git tag --sort -creatordate)
 
