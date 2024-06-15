8e2b09d492595ddbca26f752c8d998acfd9b8f37
64J0 <vinigaio97@gmail.com>
2024-06-15 15:36:50 -0300

    chore: minor fixes

--- .github/fsharp-custom-action/run.sh
+++ .github/fsharp-custom-action/run.sh
@@ -22,7 +22,7 @@ GIT_TAGS=$(git tag --sort -creatordate)
 echo "[+] GIT_TAGS: ${GIT_TAGS}"
 
 # dotnet run --project . "${GIT_TAGS}"
-/proj/main "${GIT_TAGS}"
+/proj/Main "${GIT_TAGS}"
 
 yq -i '. * load("src/workflow.new.yml")' "${REPO_PATH}.github/workflows/${WORKFLOW_FILE_NAME}"
 
